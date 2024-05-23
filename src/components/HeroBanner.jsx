413547a115ed1aa13b45e22a91e0338db2c268ac
ToniDevStyle <tvr9869@gmail.com>
2024-05-23 13:33:43 +0200

    Styling and adding the SearchExercises components

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Box, Stack, Typography, Button } from '@mui/material'
 
-import HeroBannerImage from '../assets/images/banner.png'
+import HeroBannerImage from '../assets/images/bannerWOBack.png'
 
 const HeroBanner = () => {
   return (
     <Box sx={{mt: {lg: '212px', xs: '70px'},
               ml: {sm: '50px'}
               }} position="relative" p="20px">
-      <Typography color='#FF2625' fontWeight="600" fontSize="26px">
-        Fitness Club
+      <Typography color='#e4e2dd' fontWeight="600" fontSize="26px">
+        Shell Fitness
       </Typography>
       <Typography  fontWeight={700} sx={{fontSize: {lg: '44px', xs: '40px'}}}
       mb="23px" mt="30px">
@@ -19,8 +19,9 @@ const HeroBanner = () => {
             Check out the most effective exercises
       </Typography>
       
-      <Button variant='contained' color='error'>Explore Exercises</Button>
-      <Typography fontWeight={600} color="#FF2625" sx={{opacity: 0.1, display: {lg: 'block', xs: 'none'}}}
+      <Button variant='contained'  style={ 
+              { backgroundColor: "#ff89a9", color:"black", padding: '10px'}}>Explore Exercises</Button>
+      <Typography fontWeight={600} color="#f4a7bb" sx={{opacity: 0.4, display: {lg: 'block', xs: 'none'}}}
       fontSize="200px">
         Exercise
       </Typography>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Stack } from '@mui/material';
 
-import Logo from '../assets/images/Logo.png'
+import Logo from '../assets/images/Shell.png'
 
 const Navbar = () => {
   return (
     <Stack direction="row" justifyContent="space-around" sx={{gap: { sm: '122px', xs: '40px'}, mt: {sm: '32px', xs: '20px'}, justifyContent: 'none'}} px="20px">
       <Link to="/">
-        <img src={Logo} alt='logo' style={{width: '48px', height: '48px', margin: '0 20px'}}/>
+        <img src={Logo} alt='logo' style={{width: '60px', height: '60px', margin: '0 30px'}}/>
       </Link>
       <Stack direction="row" gap="40px" fontSize="24px" alignItems="flex-end">
-        <Link to="/" style={{textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625'}}>Home</Link>
-        <a href="#exercises" style={{textDecoration: 'none', color: '#3A1212'}}>Exercises</a>
+        <Link to="/" style={{textDecoration: 'none', color: 'white', borderBottom: '3px solid white'}}>Home</Link>
+        <a href="#exercises" style={{textDecoration: 'none', color: 'white'}}>Exercises</a>
       </Stack>
     </Stack>
   )
