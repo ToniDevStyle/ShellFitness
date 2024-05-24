6fbd08f7cc65b1b11418240e5faac59e11d1968b
ToniDevStyle <tvr9869@gmail.com>
2024-05-24 09:36:18 +0200

    fully responsive implementations

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,34 +1,87 @@
-import React from 'react';
-import { Box, Stack, Typography, Button } from '@mui/material'
+import React from "react";
+import { Box, Stack, Typography, Button } from "@mui/material";
+import { useState, useEffect } from 'react';
 
-import HeroBannerImage from '../assets/images/bannerWOBack.png'
+import HeroBannerImage from "../assets/images/bannerWOBack2.png";
+import HeroSmallBannerImage from "../assets/images/Shell.png";
+
+const useScreenSize = () => {
+  const [screenSize, setScreenSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return screenSize;
+};
+
+const isBigScreen = (screenSize) => {
+  return screenSize.width >= 805;
+};
 
 const HeroBanner = () => {
+  const screenSize = useScreenSize();
+
   return (
-    <Box sx={{mt: {lg: '212px', xs: '70px'},
-              ml: {sm: '50px'}
-              }} position="relative" p="20px">
-      <Typography color='#e4e2dd' fontWeight="600" fontSize="26px">
+    <Box
+      sx={{ mt: { lg: "212px", xs: "70px" }, ml: { sm: "50px" } }}
+      position="relative"
+      p="20px"
+      className='box-hero'
+    >
+      <Typography color="#e4e2dd" fontWeight="600" fontSize="26px">
         Shell Fitness
       </Typography>
-      <Typography  fontWeight={700} sx={{fontSize: {lg: '44px', xs: '40px'}}}
-      mb="23px" mt="30px">
-            Sweat, Smile <br /> and Repeat
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "40px" } }}
+        mb="23px"
+        mt="30px"
+      >
+        Sweat, Smile <br /> and Repeat
       </Typography>
-      <Typography fontSize='22px' lineHeight="35px" mb={4}>
-            Check out the most effective exercises
+      <Typography fontSize="22px" lineHeight="35px" mb={4}>
+        Check out the most effective exercises
       </Typography>
-      
-      <Button variant='contained'  style={ 
-              { backgroundColor: "#ff89a9", color:"black", padding: '10px'}}>Explore Exercises</Button>
-      <Typography fontWeight={600} color="#f4a7bb" sx={{opacity: 0.4, display: {lg: 'block', xs: 'none'}}}
-      fontSize="200px">
+
+      <Button
+        variant="contained"
+        style={{ backgroundColor: "#ff89a9", color: "black", padding: "10px" }}
+      >
+        Explore Exercises
+      </Button>
+      <Typography
+        fontWeight={600}
+        color="#f4a7bb"
+        sx={{ opacity: 0.4, display: { lg: "block", xs: "none" } }}
+        fontSize="200px"
+      >
         Exercise
       </Typography>
-      <img src={HeroBannerImage} alt="banner" className='hero-banner-img' 
-            />
+      {
+        isBigScreen(screenSize)
+        ?
+        <img src={HeroBannerImage} alt="banner" className="hero-banner-img" />
+        :
+        <img src={HeroSmallBannerImage} alt="banner" className="hero-banner-img" />
+      }
     </Box>
-  )
-}
+  );
+};
 
-export default HeroBanner
+export default HeroBanner;
