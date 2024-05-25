3f5c4b02d09a1eca36bed819afcd0dc32a58abca
ToniDevStyle <tvr9869@gmail.com>
2024-05-25 09:47:51 +0200

    navbar margins

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,9 @@ const Navbar = () => {
   return (
     <Stack direction="row" justifyContent="space-around" sx={{gap: { sm: '122px', xs: '40px'}, mt: {sm: '32px', xs: '20px'}, justifyContent: 'none'}} px="20px">
       <Link to="/">
-        <img src={Logo} alt='logo' style={{width: '60px', height: '60px', margin: '0 30px'}}/>
+        <img src={Logo} alt='logo' style={{width: '80px', height: '80px', margin: '0 15px'}}/>
       </Link>
-      <Stack direction="row" gap="40px" fontSize="24px" alignItems="flex-end">
+      <Stack direction="row" gap="40px" fontSize="24px" alignItems="flex-end" marginBottom="25px">
         <Link to="/" style={{textDecoration: 'none', color: 'white', borderBottom: '3px solid white'}}>Home</Link>
         <a href="#exercises" style={{textDecoration: 'none', color: 'white'}}>Exercises</a>
       </Stack>
